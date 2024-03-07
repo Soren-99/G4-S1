a049cbed95c66e6c5ae26e344b4b03942d87ddfb
Persiano99 <sorenrahimi99@gmail.com>
2024-03-07 15:02:34 +0100

    Esercizio 1

---
+++ src/esercizio1/Dipendente.java
@@ -0,0 +1,35 @@
+package esercizio1;
+
+public class Dipendente {
+    private int matricola;
+    private double stipendio;
+    private Dipartimento dipartimento;
+
+    public enum Dipartimento {
+        PRODUZIONE,
+        AMMINISTRAZIONE,
+        VENDITE
+    }
+
+    public Dipendente(int matricola, double stipendio, Dipartimento dipartimento){
+        this.matricola = matricola;
+        this.stipendio = stipendio;
+        this.dipartimento = dipartimento;
+    }
+
+    public int getMatricola(){
+        return matricola;
+    }
+
+    public double getStipendio(){
+        return stipendio;
+    }
+
+    public Dipartimento getDipartimento(){
+        return dipartimento;
+    }
+
+    public void setDipartimento(Dipartimento dipartimento) {
+        this.dipartimento = dipartimento;
+    }
+}
---
+++ src/esercizio1/Main.java
@@ -0,0 +1,17 @@
+package esercizio1;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Dipendente dipendente1 = new Dipendente(1455, 1400, Dipendente.Dipartimento.PRODUZIONE);
+        Dipendente dipendente2 = new Dipendente(1748, 1550, Dipendente.Dipartimento.AMMINISTRAZIONE);
+        Dipendente dipendente3 = new Dipendente(5849, 1200, Dipendente.Dipartimento.VENDITE);
+
+        Dipendente[] dipendenti = {dipendente1, dipendente2, dipendente3};
+
+        for (Dipendente dipendente: dipendenti){
+            System.out.println("Matricola del dipendente: " + dipendente.getMatricola());
+        }
+    }
+}
